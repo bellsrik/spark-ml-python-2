e2c38e584bc536fec3c16d5824440eebb14a36e3
bellsrik <bellsrik22@gmail.com>
2017-05-04 15:22:44 -0500

    Add files via upload

---
+++ test001.py
@@ -0,0 +1,9 @@
+listofnumbers = [1,2,3,4,5,6]
+
+for number in listofnumbers:
+	print number,
+	if (number % 2 == 0):
+		print "is Even"
+	else:
+		print "is Odd"
+
---
+++ test002.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+ages = np.random.randint(18, high=90, size=500)
+print ages
